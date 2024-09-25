2874a28a1a39c2114e205e68e4f9a2098dabb8d1
Appeltysken <58435944+Appeltysken@users.noreply.github.com>
2024-09-26 00:58:28 +0300

    Update jefferson.py

--- jefferson.py
+++ jefferson.py
@@ -5,17 +5,17 @@ import string
 pygame.mixer.quit()
 pygame.init()
 
+# Константы
 WIDTH, HEIGHT = 800, 600
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Jefferson Cipher")
+pygame.display.set_caption("Шифр Джефферсона")
 
 BACKGROUND_COLOR = (240, 248, 255)  # AliceBlue
 DISK_COLOR = (176, 196, 222)  # LightSteelBlue
 TEXT_COLOR = (25, 25, 112)  # MidnightBlue
 HIGHLIGHT_COLOR = (119, 136, 153)  # LightSlateGray
-LINE_COLOR = (70, 130, 180)  # SteelBlue (линия в центре)
+LINE_COLOR = (70, 130, 180)  # SteelBlue
 LINE_ALPHA = 150  # Прозрачность линии
-
 FONT = pygame.font.SysFont('Courier', 36)
 
 class JeffersonCipher:
@@ -26,61 +26,45 @@ class JeffersonCipher:
         self.selected_disk = 0
         self.tab_pressed = False
         self.dragging_disk = None
-        self.offset_x = 0
         self.disk_order = list(range(disk_count))
         self.encrypted_message = ""
         self.encrypted_order = ""
 
-    def generate_disk(self):
+    @staticmethod
+    def generate_disk():
         return ''.join(random.sample(string.ascii_uppercase, len(string.ascii_uppercase)))
 
     def get_disk_letter(self, disk_index, offset):
         pos = (self.current_positions[disk_index] + offset) % len(string.ascii_uppercase)
         return self.disks[disk_index][pos]
 
-    def move_up(self, all_disks=False):
+    def move(self, direction, all_disks=False):
         if all_disks:
-            self.current_positions = [(pos - 1) % len(string.ascii_uppercase) for pos in self.current_positions]
+            self.current_positions = [(pos + direction) % len(string.ascii_uppercase) for pos in self.current_positions]
         else:
-            self.current_positions[self.disk_order[self.selected_disk]] = (self.current_positions[self.disk_order[self.selected_disk]] - 1) % len(string.ascii_uppercase)
+            self.current_positions[self.disk_order[self.selected_disk]] = (self.current_positions[self.disk_order[self.selected_disk]] + direction) % len(string.ascii_uppercase)
 
-    def move_down(self, all_disks=False):
-        if all_disks:
-            self.current_positions = [(pos + 1) % len(string.ascii_uppercase) for pos in self.current_positions]
-        else:
-            self.current_positions[self.disk_order[self.selected_disk]] = (self.current_positions[self.disk_order[self.selected_disk]] + 1) % len(string.ascii_uppercase)
+    def change_disk_selection(self, delta):
+        self.selected_disk = (self.selected_disk + delta) % self.disk_count
 
-    def move_left(self):
-        self.selected_disk = (self.selected_disk - 1) % self.disk_count
-
-    def move_right(self):
-        self.selected_disk = (self.selected_disk + 1) % self.disk_count
-
-    def start_drag(self, disk_index, mouse_x):
+    def start_drag(self, mouse_x):
         disk_width = WIDTH // self.disk_count - 30
         for i, disk_index in enumerate(self.disk_order):
-            x_pos = i * (disk_width + 20) + 50
-            if x_pos <= mouse_x <= x_pos + disk_width:
+            if 50 + i * (disk_width + 20) <= mouse_x <= 50 + i * (disk_width + 20) + disk_width:
                 self.dragging_disk = disk_index
-                self.offset_x = mouse_x - x_pos
                 break
 
     def stop_drag(self):
-        if self.dragging_disk is not None:
-            self.dragging_disk = None
+        self.dragging_disk = None
 
     def drag(self, mouse_x):
         if self.dragging_disk is not None:
-            
             disk_width = WIDTH // self.disk_count - 30
-            new_index = (mouse_x - 50) // (disk_width + 20)
-            new_index = max(0, min(new_index, self.disk_count - 1))
+            new_index = max(0, min((mouse_x - 50) // (disk_width + 20), self.disk_count - 1))
+            old_index = self.disk_order.index(self.dragging_disk)
 
-            if new_index != self.disk_order.index(self.dragging_disk):
-                old_index = self.disk_order.index(self.dragging_disk)
-
-                self.disk_order.pop(old_index)
-                self.disk_order.insert(new_index, self.dragging_disk)
+            if new_index != old_index:
+                self.disk_order.insert(new_index, self.disk_order.pop(old_index))
 
     def encrypt(self):
         self.encrypted_message = ''.join(self.disks[self.disk_order[i]][self.current_positions[self.disk_order[i]]] for i in range(self.disk_count))
@@ -94,38 +78,25 @@ def draw_window(cipher):
         disk_width = WIDTH // cipher.disk_count - 30
         x_pos = i * (disk_width + 20) + 50
         y_pos = HEIGHT // 2 - 100
-
-        is_selected = i == cipher.selected_disk
-        color = HIGHLIGHT_COLOR if is_selected else DISK_COLOR
+        color = HIGHLIGHT_COLOR if i == cipher.selected_disk else DISK_COLOR
 
         if cipher.dragging_disk == cipher.disk_order[i]:
             color = (150, 150, 255)
 
         pygame.draw.rect(WINDOW, color, (x_pos, y_pos, disk_width, 200), border_radius=20)
 
-        letter_y_pos_up = y_pos + 20
-        letter_y_pos_current = y_pos + 90
-        letter_y_pos_down = y_pos + 160
-
-        letter_up = cipher.get_disk_letter(cipher.disk_order[i], -1)
-        text_up = FONT.render(letter_up, True, TEXT_COLOR)
-        WINDOW.blit(text_up, (x_pos + disk_width // 2 - text_up.get_width() // 2, letter_y_pos_up))
-
-        letter_current = cipher.get_disk_letter(cipher.disk_order[i], 0)
-        text_current = FONT.render(letter_current, True, TEXT_COLOR)
-        WINDOW.blit(text_current, (x_pos + disk_width // 2 - text_current.get_width() // 2, letter_y_pos_current))
-
-        letter_down = cipher.get_disk_letter(cipher.disk_order[i], 1)
-        text_down = FONT.render(letter_down, True, TEXT_COLOR)
-        WINDOW.blit(text_down, (x_pos + disk_width // 2 - text_down.get_width() // 2, letter_y_pos_down))
+        for j, offset in enumerate([-1, 0, 1]):
+            letter = cipher.get_disk_letter(cipher.disk_order[i], offset)
+            text = FONT.render(letter, True, TEXT_COLOR)
+            WINDOW.blit(text, (x_pos + disk_width // 2 - text.get_width() // 2, y_pos + 20 + j * 70))
 
         disk_number_text = FONT.render(str(cipher.disk_order[i]), True, TEXT_COLOR)
         WINDOW.blit(disk_number_text, (x_pos + disk_width // 2 - disk_number_text.get_width() // 2, y_pos - 40))
 
-        line_y_pos = y_pos + 90 + text_current.get_height() // 2
+        # Рисуем линию
         line_surface = pygame.Surface((disk_width - 20, 4), pygame.SRCALPHA)
-        line_surface.fill((LINE_COLOR[0], LINE_COLOR[1], LINE_COLOR[2], LINE_ALPHA))
-        WINDOW.blit(line_surface, (x_pos + 10, line_y_pos))
+        line_surface.fill((*LINE_COLOR, LINE_ALPHA))
+        WINDOW.blit(line_surface, (x_pos + 10, y_pos + 90 + text.get_height() // 2))
 
     info_text = FONT.render(f"Зашифрованное сообщение: {cipher.encrypted_message}", True, TEXT_COLOR)
     order_text = FONT.render(f"Порядок дисков: {cipher.encrypted_order}", True, TEXT_COLOR)
@@ -136,8 +107,8 @@ def draw_window(cipher):
 
 def main():
     cipher = JeffersonCipher()
-
     running = True
+
     while running:
         draw_window(cipher)
 
@@ -148,34 +119,21 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_TAB:
                     cipher.tab_pressed = True
-
                 if event.key == pygame.K_UP:
-                    if cipher.tab_pressed:
-                        cipher.move_up(all_disks=True)
-                    else:
-                        cipher.move_up()
+                    cipher.move(-1, all_disks=cipher.tab_pressed)
                 elif event.key == pygame.K_DOWN:
-                    if cipher.tab_pressed:
-                        cipher.move_down(all_disks=True)
-                    else:
-                        cipher.move_down()
-
+                    cipher.move(1, all_disks=cipher.tab_pressed)
                 elif event.key == pygame.K_LEFT:
-                    cipher.move_left()
+                    cipher.change_disk_selection(-1)
                 elif event.key == pygame.K_RIGHT:
-                    cipher.move_right()
-
+                    cipher.change_disk_selection(1)
                 elif event.key == pygame.K_RETURN:
                     cipher.encrypt()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_x, mouse_y = event.pos
-                for index in range(cipher.disk_count):
-                    disk_width = WIDTH // cipher.disk_count - 30
-                    x_pos = index * (disk_width + 20) + 50
-                    y_pos = HEIGHT // 2 - 100
-                    if x_pos <= mouse_x <= x_pos + disk_width and y_pos <= mouse_y <= y_pos + 200:
-                        cipher.start_drag(index, mouse_x)
+                if HEIGHT // 2 - 100 <= mouse_y <= HEIGHT // 2 + 100:
+                    cipher.start_drag(mouse_x)
 
             if event.type == pygame.MOUSEBUTTONUP:
                 cipher.stop_drag()
